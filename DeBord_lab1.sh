64af3351a3ac473d9515c29597ac7bc9723f1278
Dylan DeBord <91545068+dylandebord729@users.noreply.github.com>
2023-08-29 11:12:51 -0400

    Add files via upload

---
+++ DeBord_lab1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Hello World! This is Dylan's first script"
+echo Quick summary of the current state:
+echo "1) current directory:"
+pwd
+echo "2) contents of current directory (including permissions):"
+ls -l
+echo "3) processes currently running here:"
+ps
+echo "4) users currently logged on and their activity:"
+w
+
